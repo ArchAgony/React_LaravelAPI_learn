7c63df619ccad46aea88ffd7f439c6a9636c39ac
ArchAgony <davaariardiansyah01@gmail.com>
2025-01-11 08:31:49 +0700

    context

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,247 +1,277 @@
 // menangani dan mengelola state dalam komponen React js
 // State adalah data yang dapat berubah seiring waktu, seperti input pengguna, status aplikasi, dll.
-import {useState} from "react";
+import { useState } from "react";
+
+// memindahkan pengguna ke rute lain secara programatis.
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  // menambahkan hooks
+  // const [formData, setFormData] = useState({
+  //     name: "",
+  //     email: "",
+  //     password: "",
+  //     password_confirmation: ""
+  // })
 
-    // menambahkan hooks
-    // const [formData, setFormData] = useState({
-    //     name: "",
-    //     email: "",
-    //     password: "",
-    //     password_confirmation: ""
-    // })
+  // const [errors, setErrors] = useState({})
 
-    // const [errors, setErrors] = useState({})
+  // const navigate = useNavigate()
 
-    // async function handleRegister(e) {
-    //     e.preventDefault()
-    //     const res = await fetch('/api/register', {
-    //      method: 'post',
-    //      body: JSON.stringify(formData)
-    //     })
-    //     const data = await res.json()
-    //     console.log(formData)
-    // }
+  // async function handleRegister(e) {
+  //     e.preventDefault()
 
-    // return (
-    //     <>
-    //         <h1 className="title">Register a new account</h1>
-    //
-    //         <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y">
-    //             <div>
-    //                 <input
-    //                 type="text"
-    //                 placeholder="Name"
-    //                 value={formData.name}
-    //                 onChange={
-    //                  (e) => setFormData({ ...formData, name: e.target.value })
-    //                 }
-    //                 />
-    //              {errors.name && <p className="error">{errors.name[0]}</p>}
-    //             </div>
-    //
-    //             <div>
-    //                 <input
-    //                 type="text"
-    //                 placeholder="Email"
-    //                 value={formData.email}
-    //                 onChange={
-    //                      (e) => setFormData({...formData, email: e.target.value})
-    //                 }
-    //                 />
-    //              {errors.email && <p className="error">{errors.email[0]}</p>}
-    //             </div>
-    //
-    //             <div>
-    //                 <input
-    //                 type="password"
-    //                 placeholder="Password"
-    //                 value={formData.password}
-    //                 onChange={
-    //                      (e) => setFormData({...formData, password: e.target.value})
-    //                 }
-    //                 />
-    //              {errors.password && <p className="error">{errors.password[0]}</p>}
-    //             </div>
-    //
-    //             <div>
-    //                 <input
-    //                 type="password"
-    //                 placeholder="Password confirmation"
-    //                 value={formData.password_confirmation}
-    //                 onChange={
-    //                      (e) => setFormData({...formData, password_confirmation: e.target.value})
-    //                 }
-    //                 />
-    //              {errors.password_confirmation && <p className="error">{errors.password_confirmation[0]}</p>}
-    //             </div>
-    //         </form>
-    //
-    //         <button className="primary-btn">Register</button>
-    //     </>
-    // )
+  //     const res = await fetch('/api/register', {
+  //      method: 'post',
+  //      body: JSON.stringify(formData)
+  //     })
 
-// ------------------------------------------------------------------------------------------------
+  //     const data = await res.json()
+  //     console.log(formData)
 
-    const [
-        // Variabel state yang menyimpan data form.
-        // contohnya adalah name: "", email: "", password: "", password_confirmation: ""
-        // dimulai dengan nilai string kosong ("")
-        formData,
+  //     if (data.errors){
+  //         setErrors(data.errors)
+  //     }else{
+  //         console.log(data)
+  //         navigate('/')
+  //         localStorage.setItem('token', data.token)
+  //     }
+  // }
 
-        // Fungsi untuk memperbarui nilai formData
-        // mengubah isi state dalam formData berdasarkan input pengguna atau aksi lainnya
-        setFormData
-    ]
-        // menambahkan state lokal pada komponen
-        = useState({
+  // ------------------------------------------------------------------------------------------------
 
-        // ini adalah variabel state formData
-        name: "",
-        email: "",
-        password: "",
-        password_confirmation: ""
-    })
+  // return (
+  //     <>
+  //         <h1 className="title">Register a new account</h1>
+  //
+  //         <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y">
+  //             <div>
+  //                 <input
+  //                 type="text"
+  //                 placeholder="Name"
+  //                 value={formData.name}
+  //                 onChange={
+  //                  (e) => setFormData({ ...formData, name: e.target.value })
+  //                 }
+  //                 />
+  //              {errors.name && <p className="error">{errors.name[0]}</p>}
+  //             </div>
+  //
+  //             <div>
+  //                 <input
+  //                 type="text"
+  //                 placeholder="Email"
+  //                 value={formData.email}
+  //                 onChange={
+  //                      (e) => setFormData({...formData, email: e.target.value})
+  //                 }
+  //                 />
+  //              {errors.email && <p className="error">{errors.email[0]}</p>}
+  //             </div>
+  //
+  //             <div>
+  //                 <input
+  //                 type="password"
+  //                 placeholder="Password"
+  //                 value={formData.password}
+  //                 onChange={
+  //                      (e) => setFormData({...formData, password: e.target.value})
+  //                 }
+  //                 />
+  //              {errors.password && <p className="error">{errors.password[0]}</p>}
+  //             </div>
+  //
+  //             <div>
+  //                 <input
+  //                 type="password"
+  //                 placeholder="Password confirmation"
+  //                 value={formData.password_confirmation}
+  //                 onChange={
+  //                      (e) => setFormData({...formData, password_confirmation: e.target.value})
+  //                 }
+  //                 />
+  //              {errors.password_confirmation && <p className="error">{errors.password_confirmation[0]}</p>}
+  //             </div>
+  //         </form>
+  //
+  //         <button className="primary-btn">Register</button>
+  //     </>
+  // )
 
-    // mendeklarasikan dan mengelola state dalam komponen fungsional
-    const [errors, setErrors] = useState({})
+  // ------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------------------
 
-    // menangani event saat pengguna mengirimkan formulir (form submission).
-    // "async" digunakan untuk menandai fungsi ini sebagai asynchronous untuk menangani pemanggilan API
-    async function handleRegister(
+  const [
+    // Variabel state yang menyimpan data form.
+    // contohnya adalah name: "", email: "", password: "", password_confirmation: ""
+    // dimulai dengan nilai string kosong ("")
+    formData,
 
-        // objek yang merepresentasikan peristiwa (event) yang terjadi, dalam hal ini pengiriman formulir.
-        e
+    // Fungsi untuk memperbarui nilai formData
+    // mengubah isi state dalam formData berdasarkan input pengguna atau aksi lainnya
+    setFormData,
+  ] =
+    // menambahkan state lokal pada komponen
+    useState({
+      // ini adalah variabel state formData
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    });
 
-    ) {
-        // mencegah halaman memuat ulang saat mengirim formulir, dikelola sepenuhnya oleh JavaScript tanpa refresh halaman.
-        // memungkinkan kita untuk menangani data secara manual
-        e.preventDefault()
+  // mendeklarasikan dan mengelola state dalam komponen fungsional
+  const [errors, setErrors] = useState({});
 
-        const res =
+  // Mendapatkan fungsi navigate dari hook useNavigate
+  // Fungsi ini digunakan untuk navigasi ke rute lain dalam aplikasi.
+  const navigate = useNavigate();
 
-            // Menunggu respons dari server sebelum melanjutkan ke baris berikutnya
-            await
+  // menangani event saat pengguna mengirimkan formulir (form submission).
+  // "async" digunakan untuk menandai fungsi ini sebagai asynchronous untuk menangani pemanggilan API
+  async function handleRegister(
+    // objek yang merepresentasikan peristiwa (event) yang terjadi, dalam hal ini pengiriman formulir.
+    e
+  ) {
+    // mencegah halaman memuat ulang saat mengirim formulir, dikelola sepenuhnya oleh JavaScript tanpa refresh halaman.
+    // memungkinkan kita untuk menangani data secara manual
+    e.preventDefault();
 
-                // melakukan permintaan HTTP ke server.
-                fetch(
+    const res =
+      // Menunggu respons dari server sebelum melanjutkan ke baris berikutnya
+      await // melakukan permintaan HTTP ke server.
+      fetch(
+        // Rute endpoint API di mana data akan dikirim
+        "/api/register",
+        {
+          // Metode HTTP untuk mengirim data
+          method: "post",
 
-            // Rute endpoint API di mana data akan dikirim
-            '/api/register', {
-
-                // Metode HTTP untuk mengirim data
-                method: 'post',
-
-                // Mengonversi objek formData menjadi string dalam format JSON
-                body: JSON.stringify(formData)
-            }
-        )
-
-        const data =
-
-            // Menunggu proses parsing JSON selesai sebelum melanjutkan.
-            await
-
-            // mengonversi respons server menjadi objek javascript yang bisa digunakan dalam kode
-            res.json()
-
-        // Menyimpan daftar kesalahan dari server untuk menampilkan pesan error validasi di formulir.
-        // mengecek variabel data dan state error
-        if (data.errors){
-
-            // Mengupdate state errors dengan nilai baru yang diambil dari data.errors.
-            setErrors(data.errors)
-
-        }else{
-
-        // melihat data yang dicetak di console
-        console.log(data)
+          // Mengonversi objek formData menjadi string dalam format JSON
+          body: JSON.stringify(formData),
         }
+      );
 
+    const data =
+      
+      // Menunggu proses parsing JSON selesai sebelum melanjutkan.
+      await 
+      
+      // mengonversi respons server menjadi objek javascript yang bisa digunakan dalam kode
+      res.json();
+
+    // Menyimpan daftar kesalahan dari server untuk menampilkan pesan error validasi di formulir.
+    // mengecek variabel data dan state error
+    if (data.errors) {
+      // Mengupdate state errors dengan nilai baru yang diambil dari data.errors.
+      setErrors(data.errors);
+    } else {
+      // melihat data yang dicetak di console
+      console.log(data);
+
+      // mengarahkan pengguna ke rute yang ditentukan ('/')
+      navigate("/");
+
+      // fitur browser untuk menyimpan data secara lokal di sisi klien
+      localStorage.
+      
+      // perintah untuk menyimpan data
+      setItem(
+
+        // Nama kunci untuk menyimpan data.
+        // 'token' adalah nama kunci yang digunakan untuk menyimpan data di localStorage
+        "token", 
+
+        // Data yang ingin disimpan
+        // data.token merujuk pada nilai token yang diperoleh dari objek data
+        data.token
+    );
     }
+  }
 
-    return (
-        <>
-            <h1 className="title">Register a new account</h1>
+  // ------------------------------------------------------------------------------------------------
 
-            <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y">
-                <div>
-                    <input type="text" placeholder="Name"
+  return (
+    <>
+      <h1 className="title">Register a new account</h1>
 
-                        /* menentukan data mana yang akan ditambah */
-                           value={formData.name}
+      <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y">
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            /* menentukan data mana yang akan ditambah */
+            value={formData.name}
+            // event handler yang dipanggil setiap kali pengguna mengetik atau mengubah nilai input.
+            onChange={(e) => // memperbarui state formData sesuai input pengguna
+              // fungsi untuk memperbarui state formData
+              setFormData(
+                // menyalin semua properti lama di dalam objek formData.
+                // tanpa spread operator, semua properti akan hilang ketika state diperbarui
+                {
+                  ...formData,
 
-                        // event handler yang dipanggil setiap kali pengguna mengetik atau mengubah nilai input.
-                           onChange=
+                  // Properti name di dalam state diperbarui dengan nilai baru
+                  // diambil dari input e.target.value
+                  name: e.target.value,
+                }
+              )
+            }
+          />
+          {/*memeriksa apakah ada pesan error untuk input bernama name di state errors*/}
+          {
+            // Merujuk pada properti name dalam objek errors
+            // Jika errors.name ada, ekspresi setelah && (elemen <p>) akan dievaluasi dan ditampilkan.
+            errors.name && (
+              // Menampilkan pesan error pertama dalam array errors.name
+              // [0] digunakan untuk mengakses elemen pertama dari array pesan error.
+              <p className="error">{errors.name[0]}</p>
+            )
+          }
+        </div>
 
-                               // memperbarui state formData sesuai input pengguna
-                               {
-                                   (e) =>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          {errors.email && <p className="error">{errors.email[0]}</p>}
+        </div>
 
-                                       // fungsi untuk memperbarui state formData
-                                       setFormData(
-                                           // menyalin semua properti lama di dalam objek formData.
-                                           // tanpa spread operator, semua properti akan hilang ketika state diperbarui
-                                           {
-                                               ...formData,
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+          {errors.password && <p className="error">{errors.password[0]}</p>}
+        </div>
 
-                                               // Properti name di dalam state diperbarui dengan nilai baru
-                                               // diambil dari input e.target.value
-                                               name: e.target.value
-                                           }
-                                       )
-                               }
-                    />
-                    {/*memeriksa apakah ada pesan error untuk input bernama name di state errors*/}
-                    {
-                        // Merujuk pada properti name dalam objek errors
-                        // Jika errors.name ada, ekspresi setelah && (elemen <p>) akan dievaluasi dan ditampilkan.
-                        errors.name &&
-
-                        // Menampilkan pesan error pertama dalam array errors.name
-                        // [0] digunakan untuk mengakses elemen pertama dari array pesan error.
-                        <p className="error">{errors.name[0]}</p>
-                    }
-                </div>
-
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={
-                            (e) => setFormData({...formData, email: e.target.value})
-                        }
-                    />
-                    {errors.email && <p className="error">{errors.email[0]}</p>}
-                </div>
-
-                <div>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={formData.password}
-                        onChange={
-                            (e) => setFormData({...formData, password: e.target.value})
-                        }
-                    />
-                    {errors.password && <p className="error">{errors.password[0]}</p>}
-                </div>
-
-                <div>
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        value={formData.password_confirmation}
-                        onChange={
-                            (e) => setFormData({...formData, password_confirmation: e.target.value})
-                        }
-                    />
-                    {errors.password_confirmation && <p className="error">{errors.password_confirmation[0]}</p>}
-                </div>
-            <button className="primary-btn">Register</button>
-            </form>
-        </>
-    )
+        <div>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={formData.password_confirmation}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                password_confirmation: e.target.value,
+              })
+            }
+          />
+          {errors.password_confirmation && (
+            <p className="error">{errors.password_confirmation[0]}</p>
+          )}
+        </div>
+        <button className="primary-btn">Register</button>
+      </form>
+    </>
+  );
 }
---
+++ src/context/appContext.jsx
@@ -0,0 +1,6 @@
+// mengelola dan berbagi data secara global dalam aplikasi React
+// tanpa mem-passing props secara manual.
+import { createContext } from "react";
+
+
+export const appContext = createContext()
